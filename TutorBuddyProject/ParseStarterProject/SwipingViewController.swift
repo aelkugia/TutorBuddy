7e823147ad6cbb07fa8c8f1926924a5beafa7be8
aelkugia <abduelkugia@gmail.com>
2017-07-19 02:44:07 -0700

    Added cocoapods/koloda for swipeView. Controller shows username/photos

--- TutorBuddyProject/ParseStarterProject/SwipingViewController.swift
+++ TutorBuddyProject/ParseStarterProject/SwipingViewController.swift
@@ -8,92 +8,219 @@
 
 import UIKit
 import Parse
+import Koloda
 
 class SwipingViewController: UIViewController {
 
-    @IBOutlet var imageView: UIImageView!
+    //@IBOutlet var imageView: UIImageView!
     
-    func wasDragged(gestureRecognizer: UIPanGestureRecognizer){
-        
-        let translation = gestureRecognizer.translation(in: view)
-        
-        let label = gestureRecognizer.view!
-        
-        label.center = CGPoint(x: self.view.bounds.width / 2 + translation.x, y: self.view.bounds.height / 2 + translation.y)
-        
-        let xFromCenter = label.center.x - self.view.bounds.width / 2
-        
-        var rotation = CGAffineTransform(rotationAngle: xFromCenter / 200)
-        
-        let scale = min(abs(100 / xFromCenter), 1)
-        
-        var stretchAndRotation = rotation.scaledBy(x: scale, y: scale)
-        
-        label.transform = stretchAndRotation
-        
-        if gestureRecognizer.state == UIGestureRecognizerState.ended {
-            
-            if label.center.x < 100 {
-                
-                print("Not chosen")
-                
-                //updateImage()
-                
-            } else if label.center.x > self.view.bounds.width - 100 {
-                
-                print("chosen")
-                
-                //updateImage()
-                
-            }
-            
-            label.center = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
-            
-            rotation = CGAffineTransform(rotationAngle: 0)
-            
-            stretchAndRotation = rotation.scaledBy(x: 1, y: 1)
-            
-            label.transform = stretchAndRotation
-            
-            label.center = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
-            
-        }
-        
-    }
-    
-//    func updateImage() {
+//    var displayedUserID = ""
+//    
+//    func wasDragged(gestureRecognizer: UIPanGestureRecognizer){
 //        
-//        //let query = PFUser.query()
+//        let translation = gestureRecognizer.translation(in: view)
 //        
-//        //query?.whereKey("isInPerson", equalTo: Any)
+//        let label = gestureRecognizer.view!
 //        
-//        let imageFile = user["photo"] as! PFFile
-//                    
-//        imageFile.getDataInBackground(block: { (data, error) in
-//                        
-//            if let imageData = data {
-//                            
-//                self.imageView.image = UIImage(data: imageData)
+//        label.center = CGPoint(x: self.view.bounds.width / 2 + translation.x, y: self.view.bounds.height / 2 + translation.y)
+//        
+//        let xFromCenter = label.center.x - self.view.bounds.width / 2
+//        
+//        var rotation = CGAffineTransform(rotationAngle: xFromCenter / 200)
+//        
+//        let scale = min(abs(100 / xFromCenter), 1)
+//        
+//        var stretchAndRotation = rotation.scaledBy(x: scale, y: scale)
+//        
+//        label.transform = stretchAndRotation
+//        
+//        if gestureRecognizer.state == UIGestureRecognizerState.ended {
+//            
+//            var acceptedOrRejected = ""
+//            
+//            if label.center.x < 100 {
+//                
+//                acceptedOrRejected = "rejected"
+//                
+//            } else if label.center.x > self.view.bounds.width - 100 {
+//                
+//                acceptedOrRejected = "accepted"
+//                
 //            }
-//                        
-//        })
+//            
+//            if acceptedOrRejected != "" && displayedUserID != "" {
+//                
+//                PFUser.current()?.addUniqueObjects(from: [displayedUserID], forKey: acceptedOrRejected)
+//                
+//                PFUser.current()?.saveInBackground(block: { (success, error) in
+//                    
+//                    print(PFUser.current())
+//                    
+//                    self.updateImage()
+//
+//                })
+//                
+//            }
+//            
+//            label.center = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
+//            
+//            rotation = CGAffineTransform(rotationAngle: 0)
+//            
+//            stretchAndRotation = rotation.scaledBy(x: 1, y: 1)
+//            
+//            label.transform = stretchAndRotation
+//            
+//            label.center = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
+//            
+//        }
 //        
+//    }
+//    
+//   func updateImage() {
+//    
+//        //let query = PFUser.query()
+////        
+////        //query?.whereKey("isInPerson", equalTo: Any)
+//    
+//        let query = PFUser.query()
+//    
+//        print(PFUser.current())
+//    
+//        //query?.whereKey("isInPerson", equalTo: (PFUser.current()?["isInPerson"])!)
+//    
+//        var ignoredUsers = [""]
+//    
+//        if let acceptedUsers = PFUser.current()?["accepted"] {
+//        
+//            ignoredUsers += acceptedUsers as! Array
+//        
+//        }
+//    
+//        if let rejectedUsers = PFUser.current()?["rejected"] {
+//        
+//            ignoredUsers += rejectedUsers as! Array
+//        
+//        }
+//    
+//        query?.whereKey("objectId", notContainedIn: ignoredUsers)
+//    
+//        query?.limit = 1
+//    
+//        query?.findObjectsInBackground(block: { (objects, error) in
+//            
+//            if let users = objects {
+//                
+//                for object in users {
+//                    
+//                    if let user = object as? PFUser {
+//                        
+//                        self.displayedUserID = user.objectId!
+//                        
+//                        let imageFile = user["photo"] as! PFFile
+//                        
+//                        imageFile.getDataInBackground(block: { (data, error) in
+//                            
+//                            if error != nil {
+//                                
+//                                print(error)
+//                                
+//                            }
+//                            
+//                            if let imageData = data {
+//                                
+//                                self.imageView.image = UIImage(data: imageData)
+//                                
+//                            }
+//                        
+//                        })
+//                    
+//                    }
+//                }
+//            
+//            }
+//        })
+//    
+//    
+//    
+////        self.displayedUserID = (PFUser.current()?.objectId!)!
+////    
+////        let imageFile = PFUser.current()?["photo"] as! PFFile
+////    
+////        imageFile.getDataInBackground(block: { (data, error) in
+////            
+////        if let imageData = data {
+////            
+////        self.imageView.image = UIImage(data: imageData)
+////            }
+////                        
+////        })
+//    
 //        
 //    }
 
+    
+    @IBOutlet weak var swipeCard: KolodaView!
+    
+    var users = [String]()
+    
+    var images = [PFFile]()
+    
+    var courses = [[String]]()
+    
+    var skills = [[String]]()
+    
+    var availability = [Bool]()
+    
+    // Research model objects
+    //1) add textviews to nib view and file
+    //2) set up area for skills, courses, availability
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        let gesture = UIPanGestureRecognizer(target: self, action: #selector(self.wasDragged(gestureRecognizer:)))
-        
-        imageView.isUserInteractionEnabled = true
-        
-        imageView.addGestureRecognizer(gesture)
+//        let gesture = UIPanGestureRecognizer(target: self, action: #selector(self.wasDragged(gestureRecognizer:)))
+//        
+//        imageView.isUserInteractionEnabled = true
+//        
+//        imageView.addGestureRecognizer(gesture)
         
         //updateImage()
+        
+        swipeCard.delegate = self
+        
+        swipeCard.dataSource = self
+        
+        let query = PFUser.query()
+        
+        query?.whereKeyExists("objectId")
+        
+        query?.findObjectsInBackground(block: { (objects, error) in
+            
+            if error == nil {
+                
+                for object in objects! {
+                    
+                    if let user = object as? PFUser{
+                        
+                        let username = user["username"] as! String
+                        
+                        let imageFile = user["photo"] as! PFFile
+                        
+                        self.images.append(imageFile)
+                        
+                        self.users.append(username)
+                        
+                        self.swipeCard.reloadData()
+                        
+                    }}
+                    
+                
+            }
+            
+        })
+        
         
         
     }
@@ -123,5 +250,81 @@ class SwipingViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
+    
 
 }
+
+
+extension SwipingViewController: KolodaViewDataSource, KolodaViewDelegate{
+    
+    func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
+        
+        return users.count
+        
+    }
+    
+    func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
+        
+        let swipeCard = Bundle.main.loadNibNamed("swipeCard", owner: self, options: nil)?[0] as? swipeCard
+        
+        let imageFile = images[index]
+        
+        imageFile.getDataInBackground(block: { (data, error) in
+            
+            if error != nil {
+                
+                print(error)
+                
+            }
+            
+            if let imageData = data {
+                
+                swipeCard?.imageView.image = UIImage(data: imageData)
+                
+                swipeCard?.label.text = self.users[index]
+                
+                
+            }
+            
+
+        })
+        
+        return swipeCard!
+
+        
+    }
+    
+    func kolodaSpeedThatCardShouldDrag(_ koloda: KolodaView) -> DragSpeed {
+        
+        return .default
+        
+    }
+    
+    func kolodaDidRunOutOfCards(_ koloda: KolodaView) {
+        
+        var swipeCard = Bundle.main.loadNibNamed("swipeCard", owner: self, options: nil)?[0] as? swipeCard
+
+        
+        swipeCard?.label.text = "No more users"
+        
+        self.swipeCard.addSubview(swipeCard!)
+    
+        
+    }
+    
+    
+
+    
+
+}
+
+
+
+
+
+
+
+
+
---
+++ TutorBuddyProject/ParseStarterProject/swipeCard.swift
@@ -0,0 +1,18 @@
+//
+//  swipeCard.swift
+//  TutorBuddy
+//
+//  Created by Abdu Elkugia on 7/18/17.
+//  Copyright © 2017 Parse. All rights reserved.
+//
+
+import UIKit
+
+class swipeCard: UIView {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var label: UILabel!
+    
+
+}
